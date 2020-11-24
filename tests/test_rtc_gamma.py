4f67ea0d82a5794f6707cd1abee1d0f896d03f9a
Andrew Johnston <asjohnston@alaska.edu>
2020-11-24 10:21:09 -0900

    adjust tolerances

--- tests/test_rtc_gamma.py
+++ tests/test_rtc_gamma.py
@@ -14,13 +14,13 @@ pytestmark = pytest.mark.golden
 _API = {'main': API_URL, 'develop': API_TEST_URL}
 
 
-def get_relative_tolerance(file_name):
+def _get_tolerances(file_name):
     tif_type = file_name.name.split('_')[-1]
     if tif_type == 'area.tif':
-        return 2e-05
+        return 2e-05, 0.0
     if tif_type in ['VV.tif', 'VH.tif', 'HH.tif', 'HV.tif']:
-        return 1e-05
-    return 1e-12
+        return 1e-05, 1e-05
+    return 0.0, 0.0
 
 
 @pytest.mark.nameskip
@@ -116,8 +116,8 @@ def test_golden_tifs(comparison_dirs):
 
             try:
                 compare.compare_raster_info(main_file, develop_file)
-                relative_tolerance = get_relative_tolerance(main_file)
-                compare.values_are_close(main_ds, develop_ds, rtol=relative_tolerance, atol=0)
+                relative_tolerance, absolute_tolerance = _get_tolerances(main_file)
+                compare.values_are_close(main_ds, develop_ds, rtol=relative_tolerance, atol=absolute_tolerance)
             except compare.ComparisonFailure as e:
                 messages.append(f'{comparison_header}\n{e}')
                 failure_count += 1
