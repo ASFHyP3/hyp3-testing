a18dd52b6fc5273bd1ad0afa0be37f04df40931c
jiangzhu <jzhu4@alaska.edu>
2023-10-02 14:57:26 -0800

    seperate mask similarity and data statistic comparison

--- hyp3_testing/compare.py
+++ hyp3_testing/compare.py
@@ -58,31 +58,39 @@ def _read_as_maskeddata(tif):
     return ma.array(band.ReadAsArray(), mask=mk_band.ReadAsArray() == 0)
 
 
-def _assert_within_statistic(main_tif, deve_tif, mask_rate, value_range_rate):
-    data_main = _read_as_maskeddata(main_tif)
-    data_deve = _read_as_maskeddata(deve_tif)
-
+def _assert_mask_similarity(reference: np.array, secondary: np.array, mask_rate: float = 0.95):
+    data_main = np.ma.masked_invalid(reference)
+    data_deve = np.ma.masked_invalid(secondary)
     # compare mask
     valid_mask_and = np.bitwise_and(~data_main.mask, ~data_deve.mask)
     valid_mask_or = np.bitwise_or(~data_main.mask, ~data_deve.mask)
     msk_rate = valid_mask_and.sum()/valid_mask_or.sum()
-
     if msk_rate <= mask_rate:
         raise AssertionError(
             f'Two masks match with less than {mask_rate}')
 
-    # compare statistic of data
-    diff = data_main - data_deve
 
+def mask_are_within_similarity(reference: np.array, secondary: np.array, mask_rate: float = 0.95):
+    try:
+        _assert_mask_similarity(reference=reference, secondary=secondary, mask_rate=mask_rate)
+    except AssertionError as e:
+        raise ComparisonFailure(
+            '\n'.join(['Values are different.', '', clarify_xr_message(str(e))])
+        )
+
+def _assert_within_statistic(reference: np.array, secondary: np.array, value_range_rate: float):
+    data_main = np.ma.masked_invalid(reference)
+    data_deve = np.ma.masked_invalid(secondary)
+    diff = data_main - data_deve
     if diff.max() != diff.min() and 6*diff.std()/(diff.max() - diff.min()) > value_range_rate:
         raise AssertionError(
             f'Not all of 99.7% elements in the data has values within {value_range_rate*100} % of the range of the data'
         )
 
 
-def values_are_within_statistic(main_tif: str, deve_tif: str, mask_rate: float = 0.95, value_range_rate: float = 0.05):
+def values_are_within_statistic(reference: np.array, secondary: np.array, mask_rate: float = 0.95, value_range_rate: float = 0.05):
     try:
-        _assert_within_statistic(main_tif, deve_tif, mask_rate=mask_rate, value_range_rate=value_range_rate)
+        _assert_within_statistic(reference=reference, secondary=secondary, value_range_rate=value_range_rate)
     except AssertionError as e:
         raise ComparisonFailure(
             '\n'.join(['Values are different.', '', clarify_xr_message(str(e))])
