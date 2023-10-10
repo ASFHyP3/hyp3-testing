8fccd7902499ec5485f6b0331c4b5d8ffc97d70e
Forrest Williams <ffwilliams2@alaska.edu>
2023-10-10 12:44:57 -0500

    remove depreceated insar test functionality, update changelog

--- hyp3_testing/compare.py
+++ hyp3_testing/compare.py
@@ -34,23 +34,6 @@ def bit_for_bit(reference: Path, secondary: Path):
         raise ComparisonFailure('Files differ at the binary level')
 
 
-def _assert_within_tolerance(reference: np.array, secondary: np.array, atol: float, n_allowable: int):
-    diff = np.ma.masked_invalid(reference) - np.ma.masked_invalid(secondary)
-    n_exceed = (~np.isclose(diff.filled(0.0), 0.0, rtol=0.0, atol=atol)).sum()
-    if n_exceed > n_allowable:
-        raise AssertionError(
-            f'Too many values are outside of the tolerance for ({n_exceed} vs {n_allowable})')
-
-
-def values_are_within_tolerance(reference: np.array, secondary: np.array, atol: float, n_allowable: int):
-    try:
-        _assert_within_tolerance(reference, secondary, atol=atol, n_allowable=n_allowable)
-    except AssertionError as e:
-        raise ComparisonFailure(
-            '\n'.join(['Values are different.', '', clarify_xr_message(str(e))])
-        )
-
-
 def _assert_mask_similarity(reference: np.array, secondary: np.array, mask_rate: float = 0.95):
     data_main = np.ma.masked_invalid(reference)
     data_deve = np.ma.masked_invalid(secondary)
