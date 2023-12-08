186463321fedbf0983cef0c9cb51e88ec9937956
Andrew Player <andrewplayer3@gmail.com>
2023-12-08 14:28:09 -0600

    added readme comparison

--- tests/test_burst_insar.py
+++ tests/test_burst_insar.py
@@ -83,8 +83,12 @@ def test_golden_burst_insar(comparison_environments, jobs_info, keep):
             compare.compare_product_files(main_dir, develop_dir)
 
             for (main_file, dev_file) in find_files_in_products(main_dir, develop_dir, pattern='*.txt'):
-                if 'README' not in str(main_file):
-                    compare.compare_parameter_files(str(main_file), str(dev_file))
+                main_file = str(main_file)
+                dev_file = str(dev_file)
+                if 'README' not in main_file:
+                    compare.compare_parameter_files(main_file, dev_file)
+                else:
+                    compare.compare_readme_files(main_file, dev_file)
 
             for main_tif, develop_tif in zip(main_tifs, develop_tifs):
                 comparison_header = '\n'.join(['-' * 80, str(main_tif), str(develop_tif), '-' * 80])
