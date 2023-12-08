dd4ee2a2871cfacdf206c833f8760bb495f39657
Andrew Player <andrewplayer3@gmail.com>
2023-12-08 12:57:59 -0600

    added initial functions to compare files + params

--- hyp3_testing/compare.py
+++ hyp3_testing/compare.py
@@ -269,3 +269,25 @@ def _find_wkt(variable: xr.Variable) -> Optional[str]:
     if wkt is None:
         wkt = variable.attrs.get('spatial_ref')
     return wkt
+
+
+def compare_product_files(main_dir: str, develop_dir: str):
+    main_files = set(Path(main_dir).iterdir())
+    develop_files = set(Path(develop_dir).iterdir())
+
+    if main_files != develop_files:
+        raise ComparisonFailure(
+            f'Product files are not the same.\n  Reference: {main_files}\n  Secondary: {develop_files}'
+        )
+
+
+def compare_parameter_files(main_parameter_file, develop_parameter_file):
+    with open(main_parameter_file, 'r') as main_parameters:
+        main_parameters = main_parameters.read()
+        with open(develop_parameter_file, 'r') as develop_parameters:
+            develop_parameters = develop_parameters.read()
+
+            if main_parameters != develop_parameters:
+                raise ComparisonFailure(
+                    f'Parameter files are not the same.\n  Reference: {main_parameters}\n  Secondary: {develop_parameters}'
+                )
