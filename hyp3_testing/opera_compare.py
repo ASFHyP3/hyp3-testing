67b6ac3be6ec360e3ae9bc41f4e4841ff9e326d6
jacquelynsmale <jacquelyn.smale@gmail.com>
2025-06-05 17:25:42 -0800

    switch to an assertion rather than raising error

--- hyp3_testing/opera_compare.py
+++ hyp3_testing/opera_compare.py
@@ -318,7 +318,4 @@ def compare_rtc_browse(file_1: Path, file_2: Path, threshold: float = 1.0):
         band2 = png2.GetRasterBand(band)
         band2_array = band2.ReadAsArray()
 
-        diff = np.abs(band1_array - band2_array)
-
-        if np.any(diff > threshold):
-            raise ValueError(f'Browse images have pixel values that differ more than {threshold}.')
+        assert np.all(np.isclose(band1_array, band2_array, atol=threshold))
